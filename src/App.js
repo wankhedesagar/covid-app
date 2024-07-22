3ffcf8483f120e887f5b2954812927b564acbf5b
sagar wankhede <swankhede639@gmail.com>
2024-07-22 13:12:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Covid from "./component/Covid";
+
+function App() {
+    return (
+      <div className="App">
+        <Covid />
+      </div>
+    );
+  }
+
+
+export default App;
---
+++ src/component/Sppiner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sppiner.css";
+
+function Sppiner() {
+  return <div className="loader"></div>;
+}
+
+export default Sppiner;
